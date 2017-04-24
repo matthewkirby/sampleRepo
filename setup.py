b98d70adee6a931933f1110b4238cd92991c3331
matthewkirby <matthewkirby@email.arizona.edu>
2017-04-24 16:35:07 -0700

    Made itno package

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='stuffGoesHere',
+      version='0.1dev',
+      description='Prints things',
+      author='Matthew Kirby',
+      author_email='matthewkirby@email.arizona.edu',
+      packages=['stuffGoesHere'],
+     )
